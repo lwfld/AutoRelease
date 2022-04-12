a1770b75d65000089cc47fa258e8433fab59c0ef
trimr <trimr@tuta.io>
2022-04-12 15:29:20 +0200

    auto release test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
-      <h1 className={styles.title}>===TEST===</h1>
+      <h1 className={styles.title}>===TEST_AUTORELEASE===</h1>
     </div>
   );
 }
