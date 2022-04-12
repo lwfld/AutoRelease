e67cc1fa3f1f615f15f61ae23c8a6aeb82711870
trimr <trimr@tuta.io>
2022-04-12 14:39:01 +0200

    CHANGE index.tsx, auto build test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>===TEST===</h1>
     </div>
   );
 }
